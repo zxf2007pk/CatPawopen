928eeda32ddb3dd4a4a33e6b83f9d1da5b2bc87d
zxf2007pk <511376651@qq.com>
2025-11-28 02:22:43 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -10,23 +10,16 @@ import tgchannel from "./spider/video/tgchannel.js";
 import douban from "./spider/video/douban.js";
 import push from "./spider/video/push.js";
 import {getCache} from "./website/sites.js";
-import _360ba from "./spider/video/_360ba.js";
 import qq from "./spider/video/qq.js";
 import iqiyi from "./spider/video/iqiyi.js";
 import symx from "./spider/video/symx.js";
 import syjc from "./spider/video/syjc.js";
-import m3u8cj from "./spider/video/m3u8cj.js";
-import lives from "./spider/video/lives.js";
 import jieyingshi from "./spider/video/jieyingshi.js";
 import jianpian from "./spider/video/jianpian.js";
 import fenmei_live from "./spider/video/fenmei_live.js";
-import huya from "./spider/video/huya.js";
-import douyu from "./spider/video/douyu.js";
-import cntv from "./spider/video/cntv.js";
 import bili from "./spider/video/bili.js";
-import appys from "./spider/video/appys.js";
 
-const spiders = [douban,duoduo,mogg,leijing,panta,wogg,zhizhen,jianpian,syjc,qq,iqiyi,bili,_360ba,lives,fenmei_live,cntv,huya,douyu,m3u8cj,appys,syjc,tgchannel,tgsou,baseset,push];
+const spiders = [douban,duoduo,mogg,leijing,panta,wogg,zhizhen,jianpian,syjc,qq,iqiyi,bili,fenmei_live,syjc,tgchannel,tgsou,baseset,push];
 const spiderPrefix = '/spider';
 
 /**
